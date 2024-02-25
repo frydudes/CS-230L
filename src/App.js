4b7e437ca49f8012271c22bde183a47b02a17211
frydudes <133801923+frydudes@users.noreply.github.com>
2024-02-25 17:00:42 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section - 001</h2>
+      <p>WVU ID: 800376251</p>
+      <p>Hi I am Chris</p>
+    </div>
     </div>
   );
 }
