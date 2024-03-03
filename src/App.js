d729c3c3d10a1b41994f761f0d83df687ffed222
frydudes <chf00005@mix.wvu.edu>
2024-03-03 11:54:30 -0500

    message for app.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="App">
+      <Navbar/>
+      <Card/>
       <h1>CS 230L</h1>
       <h2>Section - 001</h2>
       <p>WVU ID: 800376251</p>
       <p>Hi I am Chris</p>
-    </div>
     </div>
   );
 }
