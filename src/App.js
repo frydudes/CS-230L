0270216c5a989d6a4d0a6c5ac9dd4a99b51b4160
frydudes <chf00005@mix.wvu.edu>
2024-03-03 19:38:47 -0500

    updated location of card in app.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Card/> 
       <h1>CS 230L</h1>
       <h2>Section - 001</h2>
       <p>WVU ID: 800376251</p>
       <p>Hi I am Chris</p>
+      <p><Card/></p>
     </div>
   );
 }
